be61347ec9dbcdfde01878b7246feb92fb5d23aa
Cody-Boullt <48022559+Cody-Boullt@users.noreply.github.com>
2019-03-04 12:30:41 -0500

    Create HelpPage.java

---
+++ HelpPage.java
@@ -0,0 +1,46 @@
+/*
+ * Team Compilation
+ * Cody Boullt, Richard Creech, Alexandre Hoppe
+ * 03/04/2019
+ */
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+	
+@SuppressWarnings("serial")
+public class HelpPage extends JFrame
+{
+	private JButton quitGame;
+		 
+	public HelpPage()
+	{
+		super("Help Page");
+		setLayout(new FlowLayout());
+
+		quitGame = new JButton("CLOSE WINDOW");
+		add(quitGame);
+
+		quitGame.addActionListener(new ActionListener ()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				dispose();
+			}
+			
+		} );
+	}
+	
+	public static void main( String[] args )
+	{
+		HelpPage application = new HelpPage();
+		application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		application.setSize( 300, 300 );
+		application.setVisible( true );
+		 
+	} // end main
+	
+} // end HelpPage
