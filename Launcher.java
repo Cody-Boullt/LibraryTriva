6b6cecb44ded47c0681b129758f85efdea658bdc
Cody-Boullt <48022559+Cody-Boullt@users.noreply.github.com>
2019-03-04 12:41:54 -0500

    Update Launcher.java

--- Launcher.java
+++ Launcher.java
@@ -17,63 +17,57 @@ public class Launcher extends JFrame
 	 private JButton buttonOne;
 	 private JButton buttonTwo;
 	 private JButton buttonThree;
-	
-	 // constructor sets up GUI
-	   public Launcher()
-	   {
-		   
-		   //window title and frame layout
-		   super("Launcher");
-		   setLayout(new FlowLayout());
-		   
-		   //button and text		   
-		   buttonOne = new JButton("New Game");
-		   add(buttonOne); //add button to frame
-		   buttonTwo = new JButton("Help Page");
-		   add(buttonTwo);  //add button to frame
-		   buttonThree = new JButton("Quit Game");
-		   add(buttonThree);  //add button to frame
-		  
-		   //button handler 
-		   buttonOne.addActionListener(new ActionListener () {
-			   public void actionPerformed(ActionEvent e) {
+
+	 public Launcher()
+	 {
+		 super("Launcher");
+		 setLayout(new FlowLayout());
+
+		 buttonOne = new JButton("New Game");
+		 add(buttonOne);
+		 buttonTwo = new JButton("Help Page");
+		 add(buttonTwo);
+		 buttonThree = new JButton("Quit Game");
+		 add(buttonThree);
+
+		 buttonOne.addActionListener(new ActionListener ()
+		 {
+			 public void actionPerformed(ActionEvent e)
+			 {
 				 NewGame newGame = new NewGame();
 				 newGame.setSize( 400, 400 ); 
 				 newGame.setVisible(true);  
 				 dispose();
-			   }
-		   });
-		 //button handler 
-		   buttonTwo.addActionListener(new ActionListener(){
-			   
-			   public void actionPerformed(ActionEvent e){
-				   
-				   HelpPage helpPage = new HelpPage();
-				   helpPage.setSize( 400, 400 ); 
-				   helpPage.setVisible(true);  
-				   dispose();
-				   
-			   }
-		   });
-		 //button handler 
-		   buttonThree.addActionListener(new ActionListener ()
-			{
-				public void actionPerformed(ActionEvent e)
-				{
-					dispose();
-				}
-				
-			} );
-		   
-	     }
-	   
-    //main program
-	public static void main(String[] args) {
-		  Launcher application = new Launcher(); // create a new frame
-	      application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	      application.setSize( 300, 300 ); // set the size
-	      application.setVisible( true ); // show the frame
-	      
-		} // end main	
-	
-}
+			 }
+		 } );
+
+		 buttonTwo.addActionListener(new ActionListener()
+		 {
+			 public void actionPerformed(ActionEvent e)
+			 {
+				 HelpPage helpPage = new HelpPage();
+				 helpPage.setSize( 400, 400 );
+				 helpPage.setVisible(true);
+				 dispose();
+			 }
+		 } );
+
+		 buttonThree.addActionListener(new ActionListener ()
+		 {
+			 public void actionPerformed(ActionEvent e)
+			 {
+				 dispose();
+			 }
+		 } );
+	 } // end Launcher
+
+	 public static void main(String[] args)
+	 {
+		 Launcher application = new Launcher();
+	     application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	     application.setSize( 300, 300 );
+	     application.setVisible( true );
+	     
+	 } // end main	
+
+} // end class Launcher
