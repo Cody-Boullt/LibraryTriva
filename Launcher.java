d4a41c4d15a2b9f5916e530d378ca99f7bcd0a4c
Cody-Boullt <48022559+Cody-Boullt@users.noreply.github.com>
2019-03-11 00:24:45 -0400

    Update Launcher.java

--- Launcher.java
+++ Launcher.java
@@ -4,71 +4,95 @@
  * 03/04/2019
  */
 
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.JButton;
 
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 @SuppressWarnings("serial")
-public class Launcher extends JFrame 
-{
-	 private JButton newButton;
-	 private JButton helpButton;
-	 private JButton quitButon;
+public class Launcher extends JFrame{
 
-	 public Launcher()
-	 {
-		 super("Launcher");
-		 setLayout(new FlowLayout());
+	private JFrame frmLibraryTrivaCenter;
 
-		 newButton = new JButton("New Game");
-		 add(newButton);
-		 helpButton = new JButton("Help Page");
-		 add(helpButton);
-		 quitButon = new JButton("Quit Game");
-		 add(quitButon);
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Launcher window = new Launcher();
+					window.frmLibraryTrivaCenter.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-		 newButton.addActionListener(new ActionListener ()
-		 {
-			 public void actionPerformed(ActionEvent e)
-			 {
-				 Difficulty difficulty = new Difficulty();
-				 difficulty.setSize( 400, 400 ); 
-				 difficulty.setVisible(true);  
-				 dispose();
-			 }
-		 } );
+	/**
+	 * Create the application.
+	 */
+	public Launcher() {
+		initialize();
+	}
 
-		 helpButton.addActionListener(new ActionListener()
-		 {
-			 public void actionPerformed(ActionEvent e)
-			 {
-				 HelpPage helpPage = new HelpPage();
-				 helpPage.setSize( 400, 400 );
-				 helpPage.setVisible(true);
-				 dispose();
-			 }
-		 } );
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmLibraryTrivaCenter = new JFrame();
+		frmLibraryTrivaCenter.setTitle("Main Menu");
+		frmLibraryTrivaCenter.setBounds(200, 200, 450, 300);
+		frmLibraryTrivaCenter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLibraryTrivaCenter.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Welcome to the Library Trivia Center!");
+		lblNewLabel.setBounds(53, 5, 328, 24);
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frmLibraryTrivaCenter.getContentPane().add(lblNewLabel);
+		
+		JButton btnNewGame = new JButton("New Game");
+		btnNewGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Difficulty difficulty = new Difficulty();
+				difficulty.setLocation( 200, 200 );
+				difficulty.setSize( 450, 300 );
+				difficulty.setVisible(true);
+				frmLibraryTrivaCenter.dispose();
+			}
+		});
+		btnNewGame.setBounds(169, 84, 95, 23);
+		frmLibraryTrivaCenter.getContentPane().add(btnNewGame);
+		
+		JButton btnHowToPlay = new JButton("How To Play");
+		btnHowToPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HowToPlay howTo = new HowToPlay();
+				howTo.setLocation( 250, 150 );
+				howTo.setSize( 450, 300 );
+				howTo.setVisible(true);
+			}
+		});
+		btnHowToPlay.setBounds(162, 119, 110, 23);
+		frmLibraryTrivaCenter.getContentPane().add(btnHowToPlay);
+		
+		JButton btnQuitGame = new JButton("Quit Game");
+		btnQuitGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmLibraryTrivaCenter.dispose();
+			}
+		});
+		btnQuitGame.setBounds(169, 154, 95, 23);
+		frmLibraryTrivaCenter.getContentPane().add(btnQuitGame);
+	}
 
-		 quitButon.addActionListener(new ActionListener ()
-		 {
-			 public void actionPerformed(ActionEvent e)
-			 {
-				 dispose();
-			 }
-		 } );
-		 
-	 } // end Launcher
-
-	 public static void main(String[] args)
-	 {
-		 Launcher application = new Launcher();
-	     application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	     application.setSize( 300, 300 );
-	     application.setVisible( true );
-	     
-	 } // end main	
-
-} // end class Launcher
+}
