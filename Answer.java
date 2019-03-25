3d06cd3bef61b8bbabea95b6e009b6fdb32e5ded
perishrs <46854701+perishrs@users.noreply.github.com>
2019-03-25 13:43:24 -0400

    Update Answer.java

--- Answer.java
+++ Answer.java
@@ -17,19 +17,22 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
+
+
 public class Answer extends JFrame {
 
 	private JFrame frmAnswer;
-	private final JLabel lblCorrectincorrect = new JLabel("Correct/Incorrect");
+	int strike = 0;
+	int score = 0;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String grade, String topic) {
+	public static void main(String grade, String topic, String questPass, String choice) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Answer window = new Answer(grade, topic);
+					Answer window = new Answer(grade, topic, questPass, choice);
 					window.frmAnswer.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -41,14 +44,14 @@ public class Answer extends JFrame {
 	/**
 	 * Create the application.
 	 */
-	public Answer(String grade, String topic) {
-		initialize(grade, topic);
+	public Answer(String grade, String topic, String questPass, String choice) {
+		initialize(grade, topic, questPass, choice);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize(String grade, String topic) {
+	private void initialize(String grade, String topic, String questPass, String choice) {
 		frmAnswer = new JFrame();
 		frmAnswer.setTitle("Results");
 		frmAnswer.setBounds(200, 200, 450, 300);
@@ -91,20 +94,30 @@ public class Answer extends JFrame {
 		});
 		menu_1.add(menuItem_2);
 		frmAnswer.getContentPane().setLayout(null);
-		lblCorrectincorrect.setBounds(131, 29, 165, 31);
-		frmAnswer.getContentPane().add(lblCorrectincorrect);
-		lblCorrectincorrect.setFont(new Font("Eras Bold ITC", Font.PLAIN, 18));
+		
+		if(choice == questPass){
+			
+			JLabel lblCorrect = new JLabel("Correct!");
+			lblCorrect.setBounds(131, 29, 165, 31);
+			frmAnswer.getContentPane().add(lblCorrect);
+			lblCorrect.setFont(new Font("Eras Bold ITC", Font.PLAIN, 18));
+			
+			score = score + 1;
+		}
+		else {
+			
+			JLabel lblIncorrect = new JLabel("Incorrect!");
+			lblIncorrect.setBounds(131, 29, 165, 31);
+			frmAnswer.getContentPane().add(lblIncorrect);
+			lblIncorrect.setFont(new Font("Eras Bold ITC", Font.PLAIN, 18));
+			
+			strike = strike + 1;
+		}
 		
 		JLabel labelScore = new JLabel("Score:");
 		labelScore.setFont(new Font("Eras Bold ITC", Font.PLAIN, 18));
 		labelScore.setBounds(131, 107, 63, 31);
 		frmAnswer.getContentPane().add(labelScore);
-		
-		JLabel lblScoreCounter = new JLabel("X");
-		lblScoreCounter.setFont(new Font("Eras Bold ITC", Font.PLAIN, 18));
-		lblScoreCounter.setBounds(197, 107, 112, 31);
-		frmAnswer.getContentPane().add(lblScoreCounter);
-		
 		
 		JLabel lblStrikes = new JLabel("Strikes:");
 		lblStrikes.setFont(new Font("Eras Bold ITC", Font.PLAIN, 18));
@@ -122,8 +135,14 @@ public class Answer extends JFrame {
 				dispose();
 			}
 		});
+
 		
-		JLabel lblStrikeCounter = new JLabel("X");
+		JLabel lblScoreCounter = new JLabel(Integer.toString(score));
+		lblScoreCounter.setFont(new Font("Eras Bold ITC", Font.PLAIN, 18));
+		lblScoreCounter.setBounds(197, 107, 112, 31);
+		frmAnswer.getContentPane().add(lblScoreCounter);
+		
+		JLabel lblStrikeCounter = new JLabel(Integer.toString(strike));
 		lblStrikeCounter.setFont(new Font("Eras Bold ITC", Font.PLAIN, 18));
 		lblStrikeCounter.setBounds(207, 141, 103, 31);
 		frmAnswer.getContentPane().add(lblStrikeCounter);
