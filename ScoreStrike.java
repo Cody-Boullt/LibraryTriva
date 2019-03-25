0d94ec6a8848c46c3a107ffb3a4a17bb6866e9af
perishrs <46854701+perishrs@users.noreply.github.com>
2019-03-25 10:04:13 -0400

    Create ScoreStrike.java

---
+++ ScoreStrike.java
@@ -0,0 +1,18 @@
+/*
+ * Team Compilation
+ * Cody Boullt, Richard Creech, Alexandre Hoppe
+ * 03/26/2019
+ */
+
+public class ScoreStrike {
+	
+	public void score()
+	{
+		
+	}
+	
+	public void strike()
+	{
+		
+	}
+}
