707c5c0fa19402c07cf464ecc764eecdef4ddef2
Cody-Boullt <48022559+Cody-Boullt@users.noreply.github.com>
2019-03-25 12:50:17 -0400

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -7,6 +7,7 @@
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JFrame;
@@ -21,6 +22,7 @@ import javax.swing.SwingConstants;
 public class Question extends JFrame {
 
 	private JFrame frmQuestion;
+	private static String answerOne = "test1", answerTwo = "test2", answerThree = "test3", answerFour = "test4";
 
 	/**
 	 * Launch the application.
@@ -55,61 +57,72 @@ public class Question extends JFrame {
 	 */
 	private void initialize(String grade, String topic) {
 		String questPassed = QuestionArray.getQuestArray(grade, topic);
+		String ansPassed = AnswerArray.getAnsArray();
+		
+		answerOne = ansPassed;
+		answerTwo = AnswerArray.getWrgAnsOne();
+		answerThree = AnswerArray.getWrgAnsTwo();
+		answerFour = AnswerArray.getWrgAnsThree();
+		
+		shuffle();
 		
 		frmQuestion = new JFrame();
+		frmQuestion.setResizable(false);
 		frmQuestion.setTitle("Trivia");
 		frmQuestion.setBounds(200, 200, 450, 300);
 		frmQuestion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmQuestion.getContentPane().setLayout(null);
 		
 		JLabel lblQuestionHere = new JLabel(questPassed, SwingConstants.CENTER);
-		lblQuestionHere.setFont(new Font("Arial", Font.BOLD, 12));
+		lblQuestionHere.setFont(new Font("Eras Bold ITC", Font.PLAIN, 12));
 		lblQuestionHere.setBounds(10, 42, 412, 27);
 		frmQuestion.getContentPane().add(lblQuestionHere);
 		
-		JButton btnAnswer = new JButton("Answer 1");
-		btnAnswer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Answer.main(null);
-				frmQuestion.dispose();
-			}
-		});
-		btnAnswer.setFont(new Font("Arial", Font.BOLD, 12));
-		btnAnswer.setBounds(53, 80, 147, 75);
-		frmQuestion.getContentPane().add(btnAnswer);
-		
-		JButton btnAnswer_1 = new JButton("Answer 2");
+		JButton btnAnswer_1 = new JButton(answerOne);
 		btnAnswer_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Answer.main(null);
+			public void actionPerformed(ActionEvent e) {
+				//String choice = answerOne;
+				Answer.main(grade, topic/*, choice*/);
 				frmQuestion.dispose();
 			}
 		});
-		btnAnswer_1.setFont(new Font("Arial", Font.BOLD, 12));
-		btnAnswer_1.setBounds(233, 80, 147, 75);
+		btnAnswer_1.setFont(new Font("Eras Bold ITC", Font.PLAIN, 12));
+		btnAnswer_1.setBounds(53, 80, 147, 75);
+		//btnAnswer.setText(ansPassed);
 		frmQuestion.getContentPane().add(btnAnswer_1);
 		
-		JButton btnAnswer_2 = new JButton("Answer 3");
+		JButton btnAnswer_2 = new JButton(answerTwo);
 		btnAnswer_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Answer.main(null);
+			public void actionPerformed(ActionEvent arg0) {
+				Answer.main(grade, topic);
 				frmQuestion.dispose();
 			}
 		});
-		btnAnswer_2.setFont(new Font("Arial", Font.BOLD, 12));
-		btnAnswer_2.setBounds(53, 175, 147, 75);
+		btnAnswer_2.setFont(new Font("Eras Bold ITC", Font.PLAIN, 12));
+		btnAnswer_2.setBounds(233, 80, 147, 75);
 		frmQuestion.getContentPane().add(btnAnswer_2);
 		
-		JButton btnAnswer_3 = new JButton("Answer 4");
+		JButton btnAnswer_3 = new JButton(answerThree);
 		btnAnswer_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Answer.main(null);
+				Answer.main(grade, topic);
 				frmQuestion.dispose();
 			}
 		});
-		btnAnswer_3.setFont(new Font("Arial", Font.BOLD, 12));
-		btnAnswer_3.setBounds(233, 175, 147, 75);
+		btnAnswer_3.setFont(new Font("Eras Bold ITC", Font.PLAIN, 12));
+		btnAnswer_3.setBounds(53, 175, 147, 75);
 		frmQuestion.getContentPane().add(btnAnswer_3);
+		
+		JButton btnAnswer_4 = new JButton(answerFour);
+		btnAnswer_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Answer.main(grade, topic);
+				frmQuestion.dispose();
+			}
+		});
+		btnAnswer_4.setFont(new Font("Eras Bold ITC", Font.PLAIN, 12));
+		btnAnswer_4.setBounds(233, 175, 147, 75);
+		frmQuestion.getContentPane().add(btnAnswer_4);
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 434, 23);
@@ -148,5 +161,23 @@ public class Question extends JFrame {
 			}
 		});
 		menu_1.add(menuItem_2);
+	}
+	
+	// shuffle answers to provide different answer button positions
+	public static void shuffle() {
+		
+		Random rand = new Random();
+		int shuffleNum = rand.nextInt(757);
+		
+		// shuffle random number of times
+		for (int counter = 0; counter < shuffleNum; counter++)
+		{
+			String temp = answerOne;
+			answerOne = answerTwo;
+			answerTwo = answerThree;
+			answerThree = answerFour;
+			answerFour = temp;
+		}
+	
 	}
 }
